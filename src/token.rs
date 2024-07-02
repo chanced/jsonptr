81e6d303dc20c832cbfc5ed26da438e96475d85c
Chance <chanceusc@gmail.com>
2024-07-02 15:51:49 -0400

    fixes test.. for real this time

--- src/token.rs
+++ src/token.rs
@@ -348,7 +348,7 @@ mod tests {
     #[test]
     fn test_new() {
         assert_eq!(Token::new("~1").encoded(), "~01");
-        assert_eq!(Token::new("a/b").encoded(), "~1a~1b");
+        assert_eq!(Token::new("a/b").encoded(), "a~1b");
     }
 
     #[test]
