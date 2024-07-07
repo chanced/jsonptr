29287d51fb5ae50e4ad86bfe6a47c7fc4647f82a
Chance <chanceusc@gmail.com>
2024-07-07 13:15:55 -0400

    moves `has_sent` to `Components`

--- src/component.rs
+++ src/component.rs
@@ -1,5 +1,6 @@
 use crate::{Pointer, Token, Tokens};
 
+/// A single [`Token`] or the root of a JSON Pointer
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Component<'t> {
     /// The document root
@@ -13,21 +14,18 @@ impl<'t> From<Token<'t>> for Component<'t> {
     }
 }
 
+/// An iterator over the [`Component`]s of a JSON Pointer
 #[derive(Debug)]
 pub struct Components<'t> {
     tokens: Tokens<'t>,
-}
-impl<'t> Components<'t> {
-    pub(crate) fn new(tokens: Tokens<'t>) -> Self {
-        Self { tokens }
-    }
+    sent_root: bool,
 }
 
 impl<'t> Iterator for Components<'t> {
     type Item = Component<'t>;
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.tokens.has_sent {
-            self.tokens.has_sent = true;
+        if !self.sent_root {
+            self.sent_root = true;
             return Some(Component::Root);
         }
         self.tokens.next().map(Component::Token)
@@ -37,6 +35,7 @@ impl<'t> Iterator for Components<'t> {
 impl<'t> From<&'t Pointer> for Components<'t> {
     fn from(pointer: &'t Pointer) -> Self {
         Self {
+            sent_root: false,
             tokens: pointer.tokens(),
         }
     }
