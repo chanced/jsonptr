8a6593be9405342c906d184f26d577f6fc0c1896
Chance <chanceusc@gmail.com>
2024-07-09 14:15:58 -0400

    docs & links

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,30 @@
 // rustdoc + README hack: https://linebender.org/blog/doc-include
-//! <style>
-//! .rustdoc-hidden { display: none; }
-//! </style>
-//!
+//! <style>.rustdoc-hidden { display: none; }</style>
 //! [`Pointer`]: `crate::Pointer`
 //! [`PointerBuf`]: `crate::PointerBuf`
 //! [`Token`]: `crate::Token`
 //! [`Tokens`]: `crate::Tokens`
 //! [`Component`]: `crate::Component`
-//! [`Components`]: `crate::Components`
-//! [`Resolve`]: `crate::resolve::Resolve`
-//! [`ResolveMut`]: `crate::resolve::ResolveMut`
-//! [`resolve`]: `crate::resolve`
-//! [`assign`]: `crate::assign`
-//! [`delete`]: `crate::delete`
-//! [`index`]: `crate::index`
-//! [`Root`]: `crate::Component::Root`
-//! [`str`]: `str`
-//! [`String`]: `String`
-//! [`serde_json::Value`]: `serde_json::Value`
-//! [`toml::Value`]: https://docs.rs/toml/0.8/toml/enum.Value.html
-
+//! [`from_tokens`]: crate::PointerBuf::from_tokens
+// ! [`Components`]: crate::Components
+// ! [`Resolve`]: crate::resolve::Resolve
+// ! [`ResolveMut`]: crate::resolve::ResolveMut
+// ! [`resolve`]: crate::resolve
+// ! [`assign`]: crate::assign
+// ! [`delete`]: crate::delete
+// ! [`index`]: crate::index
+// ! [`Root`]: crate::Component::Root
+// ! [`str`]: str
+// ! [`String`]: String
+// ! [`serde_json::Value`]: serde_json::Value
+// ! [`Pointer::resolve`]: crate::Pointer::resolve
+// ! [`Pointer::resolve_mut`]: crate::Pointer::resolve_mut
+// ! [`Pointer::assign`]: crate::Pointer::assign
+// ! [`Pointer::delete`]: crate::Pointer::delete
+// ! [`Pointer::parse`]: crate::Pointer::parse
+// ! [`PointerBuf::parse`]: crate::PointerBuf::parse
+// ! [`PointerBuf::from_tokens`]: crate::PointerBuf::from_tokens
+// ! [`toml::Value`]: https://docs.rs/toml/0.8/toml/enum.Value.html
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 #![deny(clippy::all, clippy::pedantic)]
@@ -53,7 +57,7 @@ pub mod resolve;
 pub use resolve::{Resolve, ResolveMut};
 
 mod pointer;
-pub use pointer::{ParseError, Pointer, PointerBuf, ReplaceTokenError};
+pub use pointer::{ParseError, Pointer, PointerBuf, ReplaceError};
 
 mod token;
 pub use token::{InvalidEncodingError, Token, Tokens};
