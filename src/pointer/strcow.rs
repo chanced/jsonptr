de92ae361c23e2448fbf3bc26974c8956b56ddd4
André Sá de Mello <codasm@pm.me>
2025-02-16 23:00:45 +0000

    coverage

--- src/pointer/strcow.rs
+++ src/pointer/strcow.rs
@@ -46,3 +46,47 @@ mod private {
     impl Sealed for String {}
     impl Sealed for Cow<'_, str> {}
 }
+
+#[cfg(test)]
+mod tests {
+    use super::StrCow;
+    use alloc::{borrow::Cow, string::String};
+
+    #[test]
+    fn sanity() {
+        let str_ref = "apple";
+        let str_own = String::from("apple");
+        let cow_ref = Cow::Borrowed("apple");
+        let cow_own = Cow::<'static, str>::Owned(String::from("apple"));
+        assert_eq!(StrCow::as_ref(&str_ref), StrCow::as_ref(&str_own));
+        assert_eq!(StrCow::as_ref(&str_ref), StrCow::as_ref(&cow_ref));
+        assert_eq!(StrCow::as_ref(&str_ref), StrCow::as_ref(&cow_own));
+        assert_eq!(StrCow::as_ref(&str_own), StrCow::as_ref(&cow_ref));
+        assert_eq!(StrCow::as_ref(&str_own), StrCow::as_ref(&cow_own));
+        assert_eq!(StrCow::as_ref(&cow_ref), StrCow::as_ref(&cow_own));
+        assert_eq!(
+            StrCow::into_owned(str_ref),
+            StrCow::into_owned(str_own.clone())
+        );
+        assert_eq!(
+            StrCow::into_owned(str_ref),
+            StrCow::into_owned(cow_ref.clone())
+        );
+        assert_eq!(
+            StrCow::into_owned(str_ref),
+            StrCow::into_owned(cow_own.clone())
+        );
+        assert_eq!(
+            StrCow::into_owned(str_own.clone()),
+            StrCow::into_owned(cow_ref.clone())
+        );
+        assert_eq!(
+            StrCow::into_owned(str_own.clone()),
+            StrCow::into_owned(cow_own.clone())
+        );
+        assert_eq!(
+            StrCow::into_owned(cow_ref.clone()),
+            StrCow::into_owned(cow_own.clone())
+        );
+    }
+}
