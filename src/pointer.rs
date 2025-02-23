a6a2b52fb5b5c76bb13b576df875c73b1457d5b5
André Sá de Mello <codasm@pm.me>
2025-02-23 17:19:29 +0000

    fix overly strict lifetime bound in Default impl

--- src/pointer.rs
+++ src/pointer.rs
@@ -36,7 +36,7 @@ mod slice;
 #[cfg_attr(not(doc), repr(transparent))]
 pub struct Pointer(str);
 
-impl Default for &'static Pointer {
+impl Default for &Pointer {
     fn default() -> Self {
         Pointer::root()
     }
@@ -2341,5 +2341,14 @@ mod tests {
         let boxed: Box<Pointer> = subjectal.clone().into();
         let unboxed = boxed.into_buf();
         assert_eq!(subjectal, unboxed);
+    }
+
+    #[test]
+    fn default_lifetime_is_correct() {
+        // if this compiles, we're good
+        #[allow(dead_code)]
+        fn or_default(ptr: &Pointer) -> &Pointer {
+            Some(ptr).unwrap_or_default()
+        }
     }
 }
